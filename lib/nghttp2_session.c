30bb8aad4417133d4a12116cebe493a8cbdc2ee2
John Galbraith <john.galbraith17@gmail.com>
2023-09-24 17:51:12 -0700

    Experimental log messages

--- lib/nghttp2_session.c
+++ lib/nghttp2_session.c
@@ -7795,9 +7795,10 @@ int nghttp2_session_set_stream_user_data(nghttp2_session *session,
 int nghttp2_session_resume_data(nghttp2_session *session, int32_t stream_id) {
   int rv;
   nghttp2_stream *stream;
+  bool deferred;
   stream = nghttp2_session_get_stream(session, stream_id);
   if (stream == NULL) printf("nghttp2: no stream %d\n", stream_id);
-  bool deferred = nghttp2_stream_check_deferred_item(stream);
+  deferred = nghttp2_stream_check_deferred_item(stream);
   if (!deferred) printf("nghttp2: no deferred item %d\n", stream_id);
   if (stream == NULL || !deferred) {
     return NGHTTP2_ERR_INVALID_ARGUMENT;
